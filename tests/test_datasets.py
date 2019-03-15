2425c18545ab25d07302cf690fd1407a9d3d5de0
Fabian-Robert Stöter <fabian-robert.stoeter@audiolabs-erlangen.de>
2019-03-15 10:18:42 +0100

    split test functions

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -25,17 +25,21 @@ def dataset(request):
     return request.param
 
 
-def test_datasets(dataset):
+def test_metadata(dataset):
     name = dataset[0]
     values = dataset[1]
 
     # check for spaces in beginning or end of name
     assert ' ' not in [name[0], name[-1]]
 
-    # check if website is up
-    code = urllib.request.urlopen(values['url'], timeout=5).getcode()
-    assert code == 200
-
     # check if all required fields are there
     for field in required_fields:
         assert field in list(values.keys())
+
+
+def test_url(dataset):
+    values = dataset[1]
+
+    # check if website is up
+    code = urllib.request.urlopen(values['url'], timeout=5).getcode()
+    assert (code == 200)
