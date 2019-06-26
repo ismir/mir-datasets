46e4d40223831720b2c72dcf2eb2c15df90c0c7d
Stefan Balke <stefan.balke@jku.at>
2019-06-26 13:26:22 +0200

    next try, travis

--- .travis_dependencies.sh
+++ .travis_dependencies.sh
@@ -11,7 +11,7 @@ conda_create ()
     conda update -q conda
     conda config --add channels pypi
     conda info -a
-    deps='pip pytest fake_useragent'
+    deps='pip pytest'
 
     conda create -q -n $ENV_NAME "python=$TRAVIS_PYTHON_VERSION" $deps
     conda update --all
