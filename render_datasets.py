db621f114799fbc95509f0e8f798b4527fedd936
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-06 18:56:08 +0000

    Deploy ismir/mir-datasets to github.com/ismir/mir-datasets.git:gh-pages

--- render_datasets.py
+++ render_datasets.py
@@ -9,23 +9,42 @@ $ ./render_datasets.py mir-datasets.yaml datasets.js
 
 import argparse
 import markdown
+import joblib
 import json
 import os
+import requests
 import sys
 import yaml
 
+
 MARKDOWN_TEMPLATE = '''
-dataset |  meta data |  contents |  with audio
---- | --- | --- | ---
+status| dataset |  metadata |  contents |  with audio
+--- | --- | --- | --- | ---
 '''
 
-MARKDOWN_RECORD = ('<a title="{key}" href="{url}" target="_blank" rel="noopener">{title}</a> '
+MARKDOWN_RECORD = ('{status} | '
+                   '<a title="{key}" href="{url}" target="_blank" rel="noopener">{title}</a> '
                    '| {metadata} | {contents} | {audio}')
+
+HEALTH = {
+    0: '&#x2705;',
+    1: '&#x2620;'
+}
+
+
+def get_url_status(url):
+    try:
+        response = requests.get(url, timeout=3)
+        status_code = response.status_code
+    except (requests.exceptions.ConnectionError, requests.exceptions.ReadTimeout) as derp:
+        status_code = 666
+    return HEALTH[status_code >= 400]
 
 
 def render_one(key, record):
     title = record.pop('title', key)
 
+    status = get_url_status(record['url'])
     metadata = record.pop('metadata', '')
     if isinstance(metadata, list):
         fields = []
@@ -36,10 +55,11 @@ def render_one(key, record):
             fields.append(item)
         metadata = ', '.join(fields)
 
-    return MARKDOWN_RECORD.format(key=key, title=title, metadata=metadata, **record)
+    return MARKDOWN_RECORD.format(key=key, title=title, metadata=metadata, 
+                                  status=status, **record)
 
 
-def render(records, output_format):
+def render(records, output_format, n_jobs=-1, verbose=0):
     '''Render a number of records to the given output format.
 
     Parameters
@@ -56,7 +76,12 @@ def render(records, output_format):
         String data to write to file.
     '''
     records = sorted(records.items(), key=lambda x: x[0].lower())
-    lines = [render_one(key, record) for key, record in records]
+    
+    # Fan out
+    pool = joblib.Parallel(n_jobs=n_jobs, verbose=verbose)
+    dfx = joblib.delayed(render_one)
+    lines = pool(dfx(key, record) for key, record in records)
+
     md = MARKDOWN_TEMPLATE + '\n'.join(lines)
 
     if output_format == 'js':
@@ -80,12 +105,19 @@ if __name__ == '__main__':
     parser.add_argument("output_file",
                         metavar="output_file", type=str,
                         help="Path to rendered output.")
+    parser.add_argument("--n_jobs",
+                        metavar="n_jobs", type=int, default=1,
+                        help="Number of CPUs to use for parallelization (-1 for all).")
+    parser.add_argument("--verbose",
+                        metavar="verbose", type=int, default=0,
+                        help="Verbosity level, 0 for nothing, >1 for something.")
 
     args = parser.parse_args()
-    dataset = yaml.load(open(args.dataset_file))
+    dataset = yaml.load(open(args.dataset_file), Loader=yaml.FullLoader)
 
     output_format = os.path.splitext(args.output_file)[-1].strip('.')
     with open(args.output_file, 'w') as fp:
-        fp.write(render(dataset, output_format))
+        fp.write(render(dataset, output_format, 
+                        n_jobs=args.n_jobs, verbose=args.verbose))
 
     sys.exit(0 if os.path.exists(args.output_file) else 1)
